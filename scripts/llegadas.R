17b04dc6ef3d2037fefd854a9badda79c1ba3955
martiblc <62562807+martiblc@users.noreply.github.com>
2023-05-16 11:04:48 +0200

    Update and rename llegadas.Rmd to llegadas.R

--- scripts/llegadas.R
+++ scripts/llegadas.R
@@ -1,26 +1,6 @@
----
-title: "llegadas_dia"
-output: html_document
-date: "2023-04-17"
-editor_options: 
-  chunk_output_type: console
----
-
-# Llegadas diarias previstas Port de Barcelona
-
-El [Port de
-Barcelona](https://opendata.portdebarcelona.cat/es/dataset/arribades-i-sortides-previstes-per-avui)
-publica cada dia un archivo que recoge las llegadas previstas y en otro
-las salidas previstas. Este script los baja cada dia.
-
-## Primeros pasos
-
-```{r setup, include=FALSE}
+# Setup
 library(tidyverse)
 library(lubridate)
-```
-
-```{r descarga y filtrar}
 
 # Descargar archivo
 
@@ -30,12 +10,7 @@ llegadas <- read_csv("https://opendata.portdebarcelona.cat/dataset/342fe09b-017b
 # Seleccionar columnas 
   select(ETA, ETAUTC, ETADIA, ETAHORA, ETD, ETDUTC, ETDDIA, ETDHORA, MMSI, IMO, VAIXELLNOM, VAIXELLTIPUS, CALAT_METRES, MANEGA_METRES, ESLORA_METRES, VAIXELLBANDERANOM, VAIXELLBANDERACODI, PORTORIGENNOM, PORTORIGENCODI, PORTDESTINOM, PORTDESTICODI, TERMINALNOM, TERMINALCODI, MOLLCODI, MOLLMODULS, CONSIGNATARI, ESTOPERATIUID, ESCALAESTAT, ESCALANUM, MESINFO)
 
-```
-
-
-## Join con Thetis
-
-```{r}
+# Join con Thetis
 
 # Abrir Thetis
 thetis18_21 <-  read_csv("data/thatis/thetis18_21.csv", col_types = cols(imo_number = col_character(), annual_average_fuel_consumption_per_distance_kg_n_mile = col_number())) %>%
@@ -45,19 +20,11 @@ thetis18_21 <-  read_csv("data/thatis/thetis18_21.csv", col_types = cols(imo_num
 llegadas_final <- llegadas %>%
   left_join(thetis18_21, by = c("IMO" = "imo_number"))
 
-```
-
-
-## Guardar
-
-```{r}
-
+# Guardar
 # Definir la fecha
 hoy <- today() %>%
   stringr::str_remove_all("-")
 
 # Guardar el archivo del dia con la fecha
 write_csv(llegadas_final, paste0("data/llegadas", "llegadas_", hoy, ".csv"))
-
-```
 
